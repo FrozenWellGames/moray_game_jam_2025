a220d8c97f3f15efd839850a3524fab95ce4d7b9
richardcoldwell <frozenwellgames@gmail.com>
2025-03-07 21:45:25 +0000

    fix direction controls

--- Player/components/scripts/movement_component.gd
+++ Player/components/scripts/movement_component.gd
@@ -10,7 +10,7 @@ func handle_movement(player_body: CharacterBody2D, direction: float, delta: floa
 	var screen_size = player_body.get_viewport_rect().size
 	var radius = min(screen_size.x, screen_size.y) / 2 - 40
 	
-	angle += direction * speed * delta
+	angle -= direction * speed * delta
 
 	player_body.position.x = screen_size.x / 2 + radius * cos(angle)
 	player_body.position.y = screen_size.y / 2 + radius * sin(angle)
