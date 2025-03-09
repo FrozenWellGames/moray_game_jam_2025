9b751a16abaab7fa311770cbb51b3b1e67aed6ad
richardcoldwell <frozenwellgames@gmail.com>
2025-03-09 00:52:51 +0000

    player high score on menu

--- Main Scenes/Main Menu/main_menu.gd
+++ Main Scenes/Main Menu/main_menu.gd
@@ -2,8 +2,10 @@ extends Node2D
 
 @export_subgroup("Nodes")
 @export var game_scene: PackedScene
+@export var high_score_label:Label
 
 func _process(_delta: float) -> void:
+	high_score_label.text = ("HIGH SCORE: " + str(GameManager.high_score))
 	if Input.is_action_just_pressed("fire"):
 		_on_Button_pressed()
 
